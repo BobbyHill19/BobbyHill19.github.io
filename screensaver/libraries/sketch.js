b69d6807a6a7f6c4f25b6ec137d11f027eedcf71
BobbyHill19 <hillb813@newschool.edu>
2017-02-22 10:49:49 -0500

    screensaver

---
+++ screensaver/libraries/sketch.js
@@ -0,0 +1,30 @@
+
+
+function setup(){
+  createCanvas(windowWidth, windowHeight,  WEBGL);
+  background(230);
+}
+
+function draw(){
+  
+  var red = map(mouseX, 0, width, 0, 255);
+  var blue = map(mouseY, 0, height, 0, 255);
+  
+  
+  rotateY(frameCount * 0.01);
+
+  for(var j = 0; j < 5; j++){
+    push();
+    
+    for(var i = 0; i < 80; i++){
+      translate(sin(frameCount * 0.001 + j) * 100, 
+      sin(frameCount * 0.001 + j) * 100, i * 0.1);
+      rotateZ(frameCount * 0.0004);
+      
+      push();
+      ellipse(20, 54, 32); 
+      pop();
+    }
+    pop();
+  }
+}
