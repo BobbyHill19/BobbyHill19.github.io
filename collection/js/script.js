f7dd02f6758792513ac7f6d1b62f95550f968043
BobbyHill19 <hillb813@newschool.edu>
2017-05-02 20:23:00 -0400

    site update

---
+++ collection/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+$(".brushstroke1, .brushstroke2, .brushstroke3, .brushstroke4,.brushstroke5, .brushstroke6, .brushstroke7, .brushstroke8 ").draggable();
+
+})
+    
+   
