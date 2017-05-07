9d0bb346436c9067026e0f880489b212a187ae34
BobbyHill19 <hillb813@newschool.edu>
2017-05-07 13:41:24 -0400

    screensaver final

--- screensaver3/js/script.js
+++ screensaver3/js/script.js
@@ -1,63 +1,59 @@
 function checkTime() {
   var date = new Date();
-
   var seconds = date.getSeconds();
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  var day = date.getDay();
-  var date = date.getDate();
-
   var object = {
-    minutes: minutes,
     seconds: seconds,
-    hours: hours,
-    day: day,
-    date: date,
   }
-
   return object;
 }
 
-
 window.addEventListener("load", drawComposition);
-
 function drawComposition() {
-  window.setInterval(scaleDivBySeconds, 2000)
+  window.setInterval(scaleDivBySeconds, 1000)
 }
 
 var whichDiv = 1;
-
 function scaleDivBySeconds() {
-
   var divs = [ "first-div", "eleventh-div", "tenth-div", "ninth-div", "eight-div",
   "seventh-div", "sixth-div", "fifth-div",  "fourth-div", "third-div", 
-  "second-div", "twelfth-div" ];
+   "second-div", ];
+
+  // make an array with div names ['first-div']
+  // get the current div ID name from the array using whichDiv
+  // divs[ whichDiv ]
+  // put that in a var
+  // get the div using document.getElementById
+  // put a CSS class on the element that will fade it out
+  // element.classList.add('fadeOut')
+
+  //put a transition on the last div
+  // if whichDiv the last number:
+    // change the position to center of the screen
+    // set whichDiv to zero
+
+  var x = divs[ whichDiv ];
+  var element = document.getElementById( x )
+  console.log( element )
+  element.classList.add( "fadeOut" )
+  var time = checkTime();
+
+  //div.style.width = time.seconds * 9 + "px";
+  //console.log( time.date )
+
+  if( time.hours < 9 ) {  
+  } else {
+  }
+
+  // whichDiv = whichDiv + ;
+  if( whichDiv == 11 ) {
+    whichDiv = 0
+  }
+  if (whichDiv < 11){
+    whichDiv = whichDiv + 1;
+  }
+  if (whichDiv == 11){
+
+  }
+}
 
 
-//   var x = divs[ whichDiv ];
-//   var element = document.getElementById( x )
-//   console.log( element )
-
-//   element.classList.add( "fadeOut" )
-
-//   var time = checkTime();
-
-//   if( time.hours < 9 ) {
-    
-//   } else {
-
-//   }
-
-
-//   if( whichDiv == 11 ) {
-//     whichDiv = 0
-//   }
-
-//   if (whichDiv < 11){
-//     whichDiv = whichDiv + 1;
-//   }
-
-//   if (whichDiv == 11){
-
-//   }
-// }
